0f92b19db94f8030228bf3d1b0c356b50655c088
Bhagyashri-jad <jadhavbhagyashree325@gmail.com>
2024-08-31 14:09:37 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -49,6 +49,16 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.settribute('view','y_line');
+      elem.settribute('column-pivotes','["stock"]');
+      elem.settribute('row-pivotes','["timestamp"]');
+      elem.settribute('columns','["top_ask_price"]');
+      elem.settribute('aggregates',
+        {"stock":distinct count",
+         "top_ask_price":"avg",
+         "timestamp":"distinct count"
+        }
+      );
     }
   }
 
